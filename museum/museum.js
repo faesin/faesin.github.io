5067e10d5dd69fea839835eab0121cd93d3a4931
Rafael Fávaro <rafael.faesin@gmail.com>
2017-11-27 19:45:42 -0200

    Museum app

---
+++ museum/museum.js
@@ -0,0 +1,120 @@
+'use strict'
+
+const minimumIntensity = -70
+const numberOfStates = 12;
+const states = Array.apply(null, {length: numberOfStates}).map(Number.call, Number)
+
+// in gray codes
+// Room <NUM> - <State> = <Transition>
+const transitions = {
+  // Room 1 - 1 = 1; 2 = 2; 3 = 3
+  1: { 1: 1, 2: 2, 3: 3 },
+  2: { 1: 1, 2: 2, 3: 3 },
+  3: { 1: 1, 2: 2, 3: 3, 4: 4 },
+
+  // Room 2 - 5 = 1; 6 = 2; 3 = 3; 4 = 4
+  4: { 1: 5, 3: 3, 4: 4 },
+  5: { 1: 5, 2: 6, 4: 4 },
+
+  // Room 3 - 5 = 1; 6 = 2; 9 = 3; 7 = 4; 8 = 5; 10 = 6
+  6: { 1: 5, 2: 6, 3: 9, 4: 7, 5: 8, 6: 10 },
+  7: { 2: 6, 3: 9, 4: 7, 5: 8, 6: 10 },
+  8: { 2: 6, 3: 9, 4: 7, 5: 8 },
+  9: { 2: 6, 3: 9, 4: 7, 5: 8 },
+
+  //Room 4 - 11 = 1; 12 = 2; 7 = 4; 10 = 6
+  10: { 1: 11, 2: 12, 4: 7, 6: 10 },
+  11: { 1: 11, 2: 12, 6: 10},
+  12: { 1: 11, 2: 12}
+}
+
+let currentState = 1
+
+function toGrayCode(n) {
+  if (n < 0) throw new RangeError("cannot convert negative numbers to gray code");
+
+  return n ^ (n >>> 1)
+}
+
+function fromGrayCode(gn) {
+  if (gn < 0) throw new RangeError('gray code numbers cannot be negative')
+
+  var g = gn.toString(2).split('')
+  var b = []
+  b[0] = g[0]
+  for (var i = 1; i < g.length; i++)
+    b[i] = g[i] ^ b[i - 1]
+
+  return parseInt(b.join(""), 2)
+}
+
+function showWarning() {
+  $('#warning').show()
+}
+
+$(document).ready(() => {
+  let context, receiver
+
+  if(!navigator.getUserMedia && !navigator.webkitGetUserMedia) {
+    showWarning()
+    return
+  }
+
+  $('body').load('slides.html', function() {
+
+    const checkForMessage = () => {
+      let positive = receiver.getIntensityValues(receiver.referencePositive)[0]
+      if(positive >= minimumIntensity) {
+        let msg = receiver.lastMessage
+        console.log('msg', msg)
+
+        console.log('currentState', currentState)
+        console.log('possible transitions', transitions[currentState])
+
+        if(transitions[currentState][msg] != null) {
+          currentState = transitions[currentState][msg]
+        }
+
+        console.log('new State', currentState)
+        console.log('new transitions', transitions[currentState])
+
+        const page = /*fromGrayCode(msg)*/ currentState - 1
+        Reveal.slide(page, 0)
+      }
+
+      window.requestAnimationFrame(checkForMessage)
+    }
+
+    Reveal.addEventListener('ready', function(event) {
+      console.log('slides ready')
+      context = (new AudioMarkings()).context
+      receiver = new Receiver(() => console.log('Receiver ready'),
+                              (err) => console.error(err),
+                              context)
+
+      console.log(receiver.onChangeMessage)
+
+      receiver.onChangeMessage = (msg) => {
+        console.log('msg', msg)
+
+        console.log('currentState', currentState)
+        console.log('possible transitions', transitions[currentState])
+        if(transitions[currentState][msg] != null) {
+          currentState = transitions[currentState][msg]
+        }
+
+        console.log('new State', currentState)
+        console.log('new transitions', transitions[currentState])
+
+        const page = /*fromGrayCode(msg)*/ currentState - 1
+        Reveal.slide(page, 0)
+      }
+    })
+
+    Reveal.addEventListener('slidechanged', function(event) {
+      console.log(event.indexh, event.indexv);
+    })
+
+    Reveal.initialize()
+  })
+})
---
+++ museum/src/AudioMarkings.js
@@ -0,0 +1,41 @@
+'use strict'
+
+function AudioMarkings(context) {
+  this.context = (context instanceof AudioContext) ? context : new (window.AudioContext || window.webkitAudioContext)()
+}
+
+AudioMarkings.prototype.createSignalsArray = function(trueFundamental, fundamental, numBits, disableNormalization) {
+  //considering 1 extra bit for the reference frequency
+  const frequencyLimit = trueFundamental + (numBits*fundamental),
+        length = (frequencyLimit/fundamental)
+
+  //create a string with a number of 0s equal to numBits
+  const overheadZeros = Array(numBits).fill(0).join('')
+
+  //create sine and cosine arrays
+  const real = new Float32Array(length + 1), //+1 for the first argument of fft arrays
+        imag = new Float32Array(real.length)
+
+  //populate waveArray with periodic waves
+  let wave,
+      waveArray = [],
+      waveArrayLength = Math.pow(2, numBits)
+
+  for(let i = 0; i < waveArrayLength; i++) {
+
+    //constructs each overtone structure
+    let bitSequence = (overheadZeros + i.toString(2)).slice(-numBits)
+    bitSequence = bitSequence.split('').join('')
+
+    for(let j = 0; j < bitSequence.length; j++)
+      real[real.length - (bitSequence.length - j + 1)] = parseInt(bitSequence[j])
+
+    real[real.length - 1] = 1
+
+    //create periodic wave and populate waveArray
+    wave = this.context.createPeriodicWave(real, imag, { disableNormalization })
+    waveArray.push(wave)
+  }
+
+  return waveArray
+}
